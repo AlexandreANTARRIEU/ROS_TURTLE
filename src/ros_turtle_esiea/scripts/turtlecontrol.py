0418eb00127179a37cf5cb698e0636d51b2344ee
turtle <turtle@pop-os.localdomain>
2021-11-23 13:17:19 +0100

    ajout commentaire

--- src/ros_turtle_esiea/scripts/turtlecontrol.py
+++ src/ros_turtle_esiea/scripts/turtlecontrol.py
@@ -5,13 +5,16 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from getkey import getkey, keys
 
-
+#menu qui permet de voir quel touche est appuyer et de réagir en conséquence
 def command():
+    #défini le type de message
     commande = Twist()
 
-
+    #détection de la touche pressee
     rospy.loginfo("Direction zqsd :")
     direction = getkey()
+
+    #gestion de la direction
     if direction == "z":
         print("J'avance")
         commande.linear.x = 1.0
@@ -26,11 +29,14 @@ def command():
         commande.angular.z = -1.0
     return commande
 
+#Crée le publisher et publie les messages au topic
 def directionturtle():
+    #création du publisher
     pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
     rospy.init_node('directionturtle', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
 
+    #jeu en continu
     while not rospy.is_shutdown():
 
         commande = command()
